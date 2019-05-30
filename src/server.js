23bccf761eed6dad7c25708fdafecc8f3349a1c7
brunooomelo <utibmelo@gmail.com>
2019-05-30 19:04:38 -0300

    https

--- src/server.js
+++ src/server.js
@@ -5,10 +5,18 @@ const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const fs = require("fs");
+var options = {
+  key: fs.readFileSync("./file.pem"),
+  cert: fs.readFileSync("./file.crt")
+};
 
 const app = express();
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
+// http
+// const server = require("http").Server(app);
+// https
+const server = require("https").createServer(options, app);
+// const io = require("socket.io")(server);
 
 const database = require("./config/database");
 const port = process.env.PORT || 8000;
@@ -26,10 +34,7 @@ app.use((req, res, next) => {
   req.io = io;
   return next();
 });
-app.get("/ping", (req, res) => {
-  return res.status(200).json({ ping: "pong" });
-});
+app.get("/ping", (req, res) => res.json({ ping: "pong" }));
 app.use(require("./routes"));
-
 
 server.listen(port, () => console.log("🤘 server up 🤘"));
